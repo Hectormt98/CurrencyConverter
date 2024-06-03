a841adf068de3c83ca1ff6fb0f0c02c8452e0139
Hectormt98 <Hectormtz987@gmail.com>
2024-06-02 22:54:53 -0500

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Hector Martinez on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
